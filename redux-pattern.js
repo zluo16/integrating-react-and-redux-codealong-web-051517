cf1d2e8588866572f7b55bd865c19469c2008447
Luke Ghenco <lukeghenco@gmail.com>
2017-02-08 06:47:51 -0500

    fix syntax issues in redux-pattern.js

--- redux-pattern.js
+++ redux-pattern.js
@@ -2,30 +2,32 @@ function createStore(reducer) {
   let state;
 
   function dispatch(action) {
-    state = reducer(state, action)
-    console.log(`the state is ${state.count}`)
-    console.log(`the action is ${action.type}`)
+    state = reducer(state, action);
+    console.log(`the state is ${state.count}`);
+    console.log(`the action is ${action.type}`);
     render();
-  }
+  };
 
   function getState(){
     return state;
-  }
+  };
 
   return {
     dispatch,
     getState
-  }
-}
+  };
+};
 
-function changeCount(state = { count: 0 }, action) {
+function changeCount(state = {
+  count: 0,
+}, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
-      return { count: state.count + 1 }
+      return { count: state.count + 1 };
     default:
       return state;
-  }
-}
+  };
+};
 
 
 function render(){
@@ -38,4 +40,4 @@ let button = document.getElementById('button');
 
 button.addEventListener('click', function() {
   store.dispatch({ type: 'INCREASE_COUNT' })
-})
+});
