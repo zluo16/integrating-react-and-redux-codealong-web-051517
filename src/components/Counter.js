b9a75d3a801f0ae5242ba7dc7971837e24a8d1c4
Luke Ghenco <lukeghenco@gmail.com>
2017-02-08 06:48:31 -0500

    convert Counter component to be a stateless function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Counter extends Component {
-  render(){
-    return (<div>
+export default (props) => {
+  return (
+    <div>
       Counter Component
     </div>
-    )
-  }
-}
-
-export default Counter;
+  )
+};
