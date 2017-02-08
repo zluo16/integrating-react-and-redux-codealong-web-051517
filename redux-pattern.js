86e86f2b50421824f8e626db6e00828ed0b678f8
Luke Ghenco <lukeghenco@gmail.com>
2017-02-08 05:45:18 -0500

    fixed spacing issue for redux-pattern.js

--- redux-pattern.js
+++ redux-pattern.js
@@ -1,41 +1,41 @@
 function createStore(reducer) {
-    let state;
+  let state;
 
-    function dispatch(action) {
-      state = reducer(state, action)
-      console.log(`the state is ${state.count}`)
-      console.log(`the action is ${action.type}`)
-      render();
-    }
+  function dispatch(action) {
+    state = reducer(state, action)
+    console.log(`the state is ${state.count}`)
+    console.log(`the action is ${action.type}`)
+    render();
+  }
 
-    function getState(){
+  function getState(){
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+}
+
+function changeCount(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 }
+    default:
       return state;
-    }
-
-    return {
-      dispatch,
-      getState
-    }
   }
-
-  function changeCount(state = { count: 0 }, action) {
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return { count: state.count + 1 }
-      default:
-        return state;
-    }
-  }
+}
 
 
-  function render(){
-    let container = document.getElementById('container');
-    container.innerHTML = store.getState().count;
-  }
+function render(){
+  let container = document.getElementById('container');
+  container.innerHTML = store.getState().count;
+}
 
-  const store = createStore(changeCount);
-  let button = document.getElementById('button');
+const store = createStore(changeCount);
+let button = document.getElementById('button');
 
-  button.addEventListener('click', function() {
-    store.dispatch({ type: 'INCREASE_COUNT' })
-  })
+button.addEventListener('click', function() {
+  store.dispatch({ type: 'INCREASE_COUNT' })
+})
